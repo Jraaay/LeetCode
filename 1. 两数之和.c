46be6fee09952dea5da493679b4cb49c6fe656e7
Jraaay <1075860716@qq.com>
2020-06-29 21:08:07 +0800

    第一个版本

---
+++ 1. 两数之和.c
@@ -0,0 +1,20 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    for (int i = 0; i < numsSize; i++)
+    {
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                returnSize[0] = i;
+                returnSize[1] = j;
+                i = numsSize;
+                j = numsSize;
+            }
+        }
+    }
+    return returnSize;
+}
