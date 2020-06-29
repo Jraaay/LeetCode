0d650eb974fea7da134c52e4884aafae17c45f5a
Jraaay <1075860716@qq.com>
2020-06-29 21:15:15 +0800

    优化了一点点效率

--- 1. 两数之和.c
+++ 1. 两数之和.c
@@ -4,7 +4,7 @@
 int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
     int* vul = (int*)malloc(sizeof(int) * 2);
-    for (int i = 0; i < numsSize; i++)
+    for (int i = 0; i < numsSize - 1; i++)
     {
         for (int j = i + 1; j < numsSize; j++)
         {
