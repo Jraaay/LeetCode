0615b622d48a5b02fa84646dcb8aea836f324e1b
Jraaay <1075860716@qq.com>
2020-06-29 21:10:34 +0800

    第二个版本

--- 1. 两数之和.c
+++ 1. 两数之和.c
@@ -3,18 +3,19 @@
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
+    int* vul = (int*)malloc(sizeof(int) * 2);
     for (int i = 0; i < numsSize; i++)
     {
         for (int j = i + 1; j < numsSize; j++)
         {
             if (nums[i] + nums[j] == target)
             {
-                returnSize[0] = i;
-                returnSize[1] = j;
+                vul[0] = i;
+                vul[1] = j;
                 i = numsSize;
                 j = numsSize;
             }
         }
     }
-    return returnSize;
+    return vul;
 }
