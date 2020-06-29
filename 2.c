68430b0ddadc131bd2df47ea31c2bd8c075de36d
Jraaay <1075860716@qq.com>
2020-06-29 22:08:38 +0800

    更换了一种解题思路

--- 2.c
+++ 2.c
@@ -22,10 +22,18 @@ int main()
     struct ListNode* l1,* l2;
     l1 = (struct ListNode*)malloc(sizeof(struct ListNode));
     l2 = (struct ListNode*)malloc(sizeof(struct ListNode));
-    l1->val = 0;
-    l2->val = 0;
-    l1->next = 0;
-    l2->next = 0;
+    l1->val = 2;
+    l2->val = 5;
+    l1->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+    l2->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+    l1->next->val = 4;
+    l2->next->val = 6;
+    l1->next->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+    l2->next->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+    l1->next->next->val = 3;
+    l2->next->next->val = 4;
+    l1->next->next->next = 0;
+    l2->next->next->next = 0;
     struct ListNode* answer = addTwoNumbers(l1, l2);
     while(answer->next != 0)
     {
@@ -39,37 +47,41 @@ int main()
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
 {
-    int i = 1;
-    int num1 = 0, num2 = 0;
-    while (l1->next != 0)
-    {
-        num1 += l1->val * i;
-        i *= 10;
-        l1 = l1->next;
-    }
-    num1 += l1->val * i;
-    i = 1;
-    while (l2->next != 0)
-    {
-        num2 += l2->val * i;
-        i *= 10;
-        l2 = l2->next;
-    }
-    num2 += l2->val * i;
-    i = num1 + num2;
     struct ListNode* answer = (struct ListNode*)malloc(sizeof(struct ListNode));
-    struct ListNode* writeAnswer = answer;
-    writeAnswer->val = 0;
-    while(i != 0)
+    struct ListNode *writeanswer = answer;
+    writeanswer->next = 0;
+    writeanswer->val = 0;
+    while (l1 != 0 || l2 != 0)
     {
-        writeAnswer->val = i % 10;
-        i /= 10;
-        if (i != 0)
+        int sum = writeanswer->val;
+        if (l1)
         {
-            writeAnswer->next = (struct ListNode*)malloc(sizeof(struct ListNode));
-            writeAnswer = writeAnswer->next;
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if (l2)
+        {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        writeanswer->val = sum % 10;
+        if (l1 != 0 || l2 != 0)
+        {
+            writeanswer->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+            writeanswer = writeanswer->next;
+            writeanswer->val = sum / 10;
+        }
+        else if (sum / 10 == 0)
+        {
+            writeanswer->next = 0;
+        }
+        else
+        {
+            writeanswer->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+            writeanswer = writeanswer->next;
+            writeanswer->val = sum / 10;
+            writeanswer->next = 0;
         }
     }
-    writeAnswer->next = 0;
     return answer;
 }
