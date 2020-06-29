b2c7cd0de3f2b35d7895f52e21f6c0fa41e77450
Jraaay <1075860716@qq.com>
2020-06-29 22:26:55 +0800

    修复逻辑错误

--- 3.c
+++ 3.c
@@ -1,21 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int lengthOfLongestSubstring(char * s);
+
+int main()
+{
+    char s[] = "abcabcbb";
+    printf("%d",lengthOfLongestSubstring(s));
+    system("pause");
+    return 0;
+}
+
 int lengthOfLongestSubstring(char * s)
 {
     int countmax = 0;
     for (int i = 0; i < strlen(s); i++)
     {
         int count = 0;
-        char letter[128];
-        if (letter[s[i]]++ == 0)
+        char letter[128] = {0};
+        for (int j = i; j < strlen(s); j++)
         {
-            count++;
-        }
-        else
-        {
-            if (count > countmax)
+            if (letter[s[j]]++ == 0)
             {
-                countmax = count;
+                count++;
             }
-            i = strlen(s);
+            else
+            {
+                if (count > countmax)
+                {
+                    countmax = count;
+                }
+                j = strlen(s);
+            }
         }
     }
+    return countmax;
 }
