d7bb0adbf19313bf24419573ff506cb1f9aefe4a
Jraaay <1075860716@qq.com>
2020-06-30 14:59:29 +0800

    修复逻辑问题和大于128字符串的问题

--- 3.c
+++ 3.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-int lengthOfLongestSubstring(char * s);
+int lengthOfLongestSubstring(char *s);
 
 int main()
 {
-    char s[] = "abcabcbb";
-    printf("%d",lengthOfLongestSubstring(s));
+    char s[] = "abc";
+    printf("%d", lengthOfLongestSubstring(s));
     system("pause");
     return 0;
 }
 
-int lengthOfLongestSubstring(char * s)
+int lengthOfLongestSubstring(char *s)
 {
     int i, start = 0, count = 0, max = 0;
-    char letter[128] = {0};
+    int letter[128] = {0};
     for (i = 0; i < strlen(s); i++)
     {
         if (letter[s[i]] > start)
@@ -28,5 +28,5 @@ int lengthOfLongestSubstring(char * s)
         }
         letter[s[i]] = i + 1;
     }
-    return max;
+    return i - start > max ? i - start : max;
 }
