263a41f4c78d4b46b6cdf79820502b200be73275
Jraaay <1075860716@qq.com>
2020-06-30 15:08:45 +0800

    增加溢出处理

--- 7.c
+++ 7.c
@@ -2,6 +2,10 @@ int reverse(int x){
     int inverse = 0;
     while (x)
     {
+        if (inverse > 214748365)
+        {
+            return 0;
+        }
         inverse = inverse * 10 + x % 10;
         x /= 10;
     }
