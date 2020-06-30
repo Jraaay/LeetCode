aa0fc3ac14e8670fc206d5e4a2891a649c277219
Jraaay <1075860716@qq.com>
2020-06-30 23:02:46 +0800

    增加记忆化

--- 198.c
+++ 198.c
@@ -1,4 +1,18 @@
+int rem_money[1000];
+
+int *start = 0;
+
 int rob(int *nums, int numsSize)
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        rem_money[i] = -1;
+    }
+    start = nums;
+    return real_rob(nums, numsSize);
+}
+
+int real_rob(int *nums, int numsSize)
 {
     if (numsSize == 0)
     {
@@ -21,10 +35,14 @@ int rob(int *nums, int numsSize)
     }
     else
     {
+        if (nums - start < 1000 && rem_money[nums - start] != -1)
+        {
+            return rem_money[nums - start]; 
+        }
         int max = 0;
         for (int i = 1; i < numsSize; i++)
         {
-            int money = rob(&nums[i], numsSize - i);
+            int money = real_rob(&nums[i], numsSize - i);
             if (i > 1)
             {
                 money += nums[0];
@@ -33,6 +51,10 @@ int rob(int *nums, int numsSize)
             {
                 max = money;
             }
+        }
+        if (nums - start < 1000)
+        {
+            rem_money[nums - start] = max;
         }
         return max;
     }
