4c8137806e7805a30ce0d00d1e71e1e35b4a2d73
Jraaay <1075860716@qq.com>
2020-06-30 20:57:54 +0800

    第一个版本

---
+++ 21.c
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
+{
+    struct ListNode* answer = NULL;
+    struct ListNode* writeanswer;
+    while (l1 && l2)
+    {
+        if (l1->val < l2->val)
+        {
+            if (answer)
+            {
+                writeanswer->next = l1;
+                writeanswer = writeanswer->next;
+            }
+            else
+            {
+                answer = l1;
+                writeanswer = l1;
+            }
+            l1 = l1->next;
+        }
+        else
+        {
+            if (answer)
+            {
+                writeanswer->next = l2;
+                writeanswer = writeanswer->next;
+            }
+            else
+            {
+                answer = l2;
+                writeanswer = l2;
+            }
+            l2 = l2->next;
+        }
+    }
+    if (l1)
+    {
+        writeanswer->next = l1;
+    }
+    else
+    {
+        writeanswer->next = l2;
+    }
+    return answer;
+}
